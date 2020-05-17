3719d57775bc4f6a561c0c0e4144682c4b89ee60
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-17 17:13:28 +0300

    HW1 task1 done
    Задача №1 сделана

---
+++ js-features/countdown/task.js
@@ -0,0 +1,63 @@
+"use strict";
+
+/*
+//Основное задание
+const counterId = setInterval(() => {
+let counterTimer = document.getElementById("timer");
+  if (counterTimer.textContent == 0) {
+    alert("Вы победили в конкурсе!");
+    clearInterval(counterId);
+    location.assign("https://www.mozilla.org/ru/firefox/download/thanks/");
+  } else {
+    counterTimer.textContent--;
+  }
+}, 100);//ускорение времени = true
+*/
+
+//Дополнительное задание №1,2
+
+let hh = 1;
+let mm = 2;
+let ss = 0;//задаем начальное значение счетчика чч:мм:сс
+
+const counterId = setInterval(() => {
+  let counterTimer = document.getElementById("status");//писать здесь id "timer" нет смысла ИМХО, ибо получается "До окончания конкурса осталось секунд: 00ч:00м:00с", а тут уже не только секунды.
+  let hh1, mm1, ss1;
+  if (ss == 0) {
+    if (mm == 0) {
+      if (hh == 0) {
+        clearInterval(counterId);
+        alert("Да свершится загрузка!");
+        location.assign("https://download.mozilla.org/?product=firefox-stub&os=win&lang=ru");
+      } else {
+        hh--;
+        mm = 59;
+      }
+    } else {
+      mm--;
+      ss = 59;
+    }
+  } else {
+    ss--;
+  }
+
+  if (hh < 10) {
+    hh1 = "0" + hh;
+  } else {
+    hh1 = hh;
+  }
+  if (mm < 10) {
+    mm1 = "0" + mm;
+  } else {
+    mm1 = mm;
+  }
+  if (ss < 10) {
+    ss1 = "0" + ss;
+  } else {
+    ss1 = ss;
+  }
+
+  counterTimer.textContent = `До окончания конкурса осталось: ${hh1}ч:${mm1}м:${ss1}с`;
+  //console.log(`${hh1}:${mm1}:${ss1}`);
+
+}, 10);//ускорение времени = true
