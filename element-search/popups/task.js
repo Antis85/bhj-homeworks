971ab36660044c5a6d3aebf6bc031147d7bea9bb
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-19 21:46:48 +0300

    HW1.2 task1 done
    Задача №1 сделана

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const modalWindowShow = document.getElementById("modal_main");
+modalWindowShow.className += " modal_active";
+
+const modalWindowsClose = document.querySelectorAll("div.modal__close");
+for (let close in modalWindowsClose) {
+  modalWindowsClose[0].onclick = () => {
+    modalWindowShow.className = "modal";
+  };
+  modalWindowsClose[1].onclick = () => {
+    modalWindowShow.className = "modal";
+    modalSuccessWindowShow.className = "modal";
+  };
+}
+
+const modalSuccessWindowShow = document.getElementById("modal_success");
+const showSuccess = document.querySelector("a.show-success");
+showSuccess.onclick = () => {
+  modalSuccessWindowShow.className += " modal_active";
+};
