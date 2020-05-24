edeecd410fe74301a1e6ee4b92ca266c98f89e14
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-25 00:04:46 +0300

    HW1.2 task1 fix1 done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,22 @@
 "use strict";
 
 const modalWindowShow = document.getElementById("modal_main");
-modalWindowShow.className += " modal_active";
+modalWindowShow.classList.add("modal_active");
 
 const modalWindowsClose = document.querySelectorAll("div.modal__close");
 modalWindowsClose[0].onclick = () => {
-  modalWindowShow.className = "modal";
+  modalWindowShow.classList.remove("modal_active");
 };
 modalWindowsClose[1].onclick = () => {
-  modalWindowShow.className = "modal";
-  modalSuccessWindowShow.className = "modal";
+  modalWindowShow.classList.remove("modal_active");
+  modalSuccessWindowShow.classList.remove("modal_active");
 };
 
 const modalSuccessWindowShow = document.getElementById("modal_success");
 const showSuccess = document.querySelector("a.show-success");
 showSuccess.onclick = () => {
-  modalSuccessWindowShow.className += " modal_active";
+  modalWindowShow.classList.remove("modal_active");
+  modalSuccessWindowShow.classList.add("modal_active");  
 };
+
 
