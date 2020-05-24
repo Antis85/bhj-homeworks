5c9f4149421a215dd1df1cbab275b300190c8b39
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-25 01:52:30 +0300

    HW1.2 task3 fix1 done

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,31 +2,36 @@
 
 const leftArrowPrev = document.querySelector(".slider__arrow_prev");
 const rightArrowNext = document.querySelector(".slider__arrow_next");
-const sliderItems = document.querySelectorAll(".slider__item");
+const sliderItems = Array.from(document.querySelectorAll(".slider__item"));
 let currentSlideNumber = 0;
+
+function setSliderItem(slideNumber) {
+  sliderItems[slideNumber].classList.add("slider__item_active");
+  sliderItems.forEach((item, index) => {
+    if (index !== slideNumber) {
+      sliderItems[index].classList.remove("slider__item_active");
+    }
+  })
+}
 
 rightArrowNext.onclick = () => {  
   if (currentSlideNumber < sliderItems.length - 1) {    
     currentSlideNumber++;
-    sliderItems.item(currentSlideNumber).className += " slider__item_active";
-    sliderItems.item(currentSlideNumber - 1).className = "slider__item";
+    setSliderItem(currentSlideNumber);    
   }
-  else {    
+  else {   
     currentSlideNumber = 0;
-    sliderItems.item(sliderItems.length - 1).className = "slider__item";
-    sliderItems.item(currentSlideNumber).className += " slider__item_active";
-  } 
+    setSliderItem(currentSlideNumber);    
+  }  
 }
 
 leftArrowPrev.onclick = () => {  
-  if (currentSlideNumber > 0) {
+  if (currentSlideNumber > 0) {    
     currentSlideNumber--;
-    sliderItems.item(currentSlideNumber).className += " slider__item_active";
-    sliderItems.item(currentSlideNumber + 1).className = "slider__item";
+    setSliderItem(currentSlideNumber);    
   }
-  else {    
+  else {
     currentSlideNumber = 4;
-    sliderItems.item(sliderItems.length - 1 - currentSlideNumber).className = "slider__item";
-    sliderItems.item(currentSlideNumber).className += " slider__item_active";
+    setSliderItem(currentSlideNumber);   
   }
 }
