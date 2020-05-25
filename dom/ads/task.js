14e0f5c2e0add09e7337e5c1c0995e0022e250c8
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-26 00:55:53 +0300

    HW2.1 task2 done

---
+++ dom/ads/task.js
@@ -0,0 +1,24 @@
+"use strict";
+console.log("test");
+
+const rotatorCases = document.querySelectorAll(".rotator__case");
+let currentCaseNumber = 0;
+
+function setRotatorCase(caseNumber) {
+  rotatorCases[caseNumber].classList.add("rotator__case_active");
+  rotatorCases.forEach((item, index) => {
+    if (index !== caseNumber) {
+      rotatorCases[index].classList.remove("rotator__case_active");
+    }
+  })
+}
+
+const rotation = setInterval(() => {
+  if (currentCaseNumber < rotatorCases.length - 1) {
+    currentCaseNumber++;
+    setRotatorCase(currentCaseNumber);
+  } else {
+    currentCaseNumber = 0;
+    setRotatorCase(currentCaseNumber);
+  }
+}, 1000);
