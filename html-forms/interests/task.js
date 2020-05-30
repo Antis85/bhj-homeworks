808644b64ebff1b746c73a9ab1d3c3bd44e5ae1a
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-05-30 19:42:49 +0300

    HW2.2 task2 done

---
+++ html-forms/interests/task.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const allInterests = document.querySelectorAll(".interest__check");
+
+for (let interest of allInterests) {
+  const subInterests = interest.closest(".interest").querySelectorAll(".interest__check");
+  interest.addEventListener("click", () => {
+    if (interest.checked) {
+      if (subInterests) {        
+        for (let subInterest of subInterests) {
+          subInterest.checked = true;
+        }
+      }      
+    } else {
+      if (subInterests) {
+        for (let subInterest of subInterests) {
+          subInterest.checked = false;
+        }
+      }
+    }
+  }
+  );
+}
